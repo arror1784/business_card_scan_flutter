c6fb5502f1ffda40f8874941da5377a0a52a65c6
arror1784 <arror1784@gmail.com>
2021-10-28 15:48:14 +0900

    add camera route

--- lib/camera_widget.dart
+++ lib/camera_widget.dart
@@ -2,32 +2,35 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:camera/camera.dart';
 
-
 class CameraApp extends StatefulWidget {
+  final List<CameraDescription> cameras;
+  const CameraApp({
+    Key? key,
+    required this.cameras,
+  }) : super(key: key);
+
   @override
   _CameraAppState createState() => _CameraAppState();
 }
 
 class _CameraAppState extends State<CameraApp> {
   late CameraController controller;
-  late List<CameraDescription> cameras;
 
   @override
-  void initState() async{
-    super.initState();
-    cameras = await availableCameras();
-    controller = CameraController(cameras[0], ResolutionPreset.max);
+  void initState(){
+    controller = CameraController(widget.cameras[0], ResolutionPreset.max);
     controller.initialize().then((_) {
       if (!mounted) {
         return;
       }
       setState(() {});
     });
+    super.initState();
   }
-
+  
   @override
   void dispose() {
-    controller?.dispose();
+    controller.dispose();
     super.dispose();
   }
 
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/material.dart';
 import 'bc_list_widget.dart';
+import 'camera_widget.dart';
+import 'package:camera/camera.dart';
 
-void main() {
+List<CameraDescription> cameras = <CameraDescription>[];
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  cameras = await availableCameras();
   runApp(const MyApp());
 }
 
@@ -14,19 +20,32 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      initialRoute: '/',
+      routes: {
+        '/': (context) => HomePage(),
+        '/camera': (context) => CameraApp(cameras: cameras,),
+      },
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("BC scan"),
-        ),
-        body: BCListWidget(),
-        floatingActionButton: FloatingActionButton.extended(
-          label: Text("ADD"),
-          icon: Icon(Icons.camera_alt_outlined),
-          onPressed: () => {},
-        ),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("BC scan"),
+      ),
+      body: BCListWidget(),
+      floatingActionButton: FloatingActionButton.extended(
+        label: Text("ADD"),
+        icon: Icon(Icons.camera_alt_outlined),
+        onPressed: () => Navigator.pushNamed(context, '/camera'),
       ),
     );
   }
